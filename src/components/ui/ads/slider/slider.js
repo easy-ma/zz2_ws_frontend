076d002403b8a0372950f891750cd54d5a0c1fcd
Xerstom <xerstomdev@outlook.fr>
2021-03-02 19:42:24 +0100

    feat: home page + ads displayer

---
+++ src/components/ui/ads/slider/slider.js
@@ -0,0 +1,43 @@
+import { Flex,Button, Box } from '@chakra-ui/react';
+import React,{useState} from 'react';
+import Ad from "../ad/ad"
+
+import Fade from 'react-reveal/Fade';
+// import Flip from 'react-reveal/Flip'
+
+const Slider = props => {
+
+    const {ads,dispatch,leftButton,rightButton} = props;
+    const [left,setLeft] = useState(true);
+
+    return (
+        <Flex mt="150px" justifyContent="center" w="80vw">
+            {leftButton
+                ? <Button zIndex="1000" onClick={
+                    () => {
+                        dispatch({type:'decrement'});
+                        setLeft(true);
+                    }
+                }
+                > {"<<"} </Button>
+            : ""
+            }
+                <Fade  left={left}  right={!left} spy={ads} >
+                    {ads?.map(ad => <Ad ad={ad}></Ad> )}
+                </Fade >
+            {rightButton 
+                ? <Button  onClick={
+                    () => {
+                        dispatch({type:'increment'});
+                        setLeft(false);
+                        }
+                    }
+                    > {">>"} </Button>
+                : ""
+        }
+
+        </Flex>
+    );
+};
+
+export default Slider;
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
 import SearchInput from "../components/ui/form/items/searchInput";
-// import RLink from "../ ui/links/routerLink";
 import RLink from "../components/ui/links/routerLink";
 import { IconButton } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import Ads from "../components/ui/ads/ads"
+
+
 export default function HomePage(props) {
   const [value, setValue] = useState("");
+
   const handleChange = (e) => {
     setValue(e.target.value);
   };
+
   return (
     <div className="wrapperDisplay">
-      <RLink to="/ads/add">
-        <IconButton aria-label="Search database" icon={<AddIcon />} />
-      </RLink>
-      <div className="search">
-        <SearchInput handleChange={handleChange} value={value} />
-      </div>
-      <div className="result">
-        <p>{value}</p>
-      </div>
+        <RLink to="/ads/add">
+            <IconButton aria-label="Search database" icon={<AddIcon />} />
+        </RLink>
+        <div className="search">
+            <SearchInput handleChange={handleChange} value={value} />
+        </div>
+        <div className="result">
+            <p>{value}</p>
+        </div>
+        <Ads searchValue={value} endPoint="/ads/get-all"></Ads>
     </div>
   );
 }
