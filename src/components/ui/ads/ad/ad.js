10af23087abc8547492e604ec93dc3683de2f749
Khaaz <khaaz.dev@gmail.com>
2021-03-10 00:22:46 +0100

    fix: ui card with location

--- src/components/ui/ads/ad/ad.js
+++ src/components/ui/ads/ad/ad.js
@@ -14,13 +14,14 @@ const Ad = (props) => {
   return (
     <Flex
       w="18vw"
+      h="55vh"
       flexDirection="column"
       boxShadow="xl"
       ml="1vw"
       mr="1vw"
       mb="5vw"
     >
-      <Box w="auto" h="50%">
+      <Box w="auto" minHeight="40%">
         <Image
           w="100%"
           h="100%"
@@ -31,30 +32,36 @@ const Ad = (props) => {
           overflow="hidden"
         />
       </Box>
-      <Flex flexDirection="column" h="50%" justifyContent="space-between" p={6}>
-        <Box h="30%">
+      <Flex
+        flexDirection="column"
+        minHeight="60%"
+        justifyContent="space-between"
+        p="1.2vw"
+      >
+        <Box>
           <Heading as="h3" fontSize="1.1vw" mb="1vh">
             {ad.name}
           </Heading>
-          <Divider></Divider>
-          <Flex flexWrap="wrap" alignItems="baseline">
-            <Text
-              color="teal"
-              fontWeight="semibold"
-              fontSize="0.8vw"
-              mr="0.5vw"
-            >
-              {ad.location}
-            </Text>
-            <Text color="teal" fontWeight="bold" fontSize="1vw">
-              {ad.city}
-            </Text>
-          </Flex>
         </Box>
+        <Divider></Divider>
+        <Flex flexWrap="wrap" alignItems="baseline">
+          <Text
+            overflowWrap="anywhere"
+            color="teal"
+            fontWeight="semibold"
+            fontSize="0.8vw"
+            mr="0.5vw"
+          >
+            {ad.location}
+          </Text>
+          {/* <Text color="teal" fontWeight="bold" fontSize="1vw">
+              {ad.city}
+            </Text> */}
+        </Flex>
         <Text
           textAlign="justify"
           lineHeight="center"
-          mt={2}
+          mt="0.6vw"
           fontSize="0.7vw"
           h="35%"
         >
@@ -65,7 +72,9 @@ const Ad = (props) => {
           h="4vh"
           color="white"
           fontSize="1vw"
-          mt={4}
+          mt="1.2vw"
+          minHeight="4vh"
+          justifySelf="flex-end"
           onClick={props.zoom}
         >
           See in details
